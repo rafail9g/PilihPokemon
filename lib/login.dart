d256d8498f08d0ee13ed4cee7f042a2eb7190d90
rafail9g <rafaaulia668@gmail.com>
2026-04-09 20:48:50 +0700

    pertama1

--- lib/login.dart
+++ lib/login.dart
@@ -40,38 +40,68 @@ class _LoginScreenState extends State<LoginScreen> {
 
             const SizedBox(height: 20),
 
-            TextField(
-              controller: user,
-              decoration: const InputDecoration(
-                labelText: "Username",
-                border: OutlineInputBorder(),
-              ),
-            ),
+            InputField("Username", user),
 
             const SizedBox(height: 15),
 
-            TextField(
-              controller: pass,
-              obscureText: hide,
-              decoration: InputDecoration(
-                labelText: "Password",
-                border: const OutlineInputBorder(),
-                suffixIcon: IconButton(
-                  icon: Icon(hide ? Icons.visibility_off : Icons.visibility),
-                  onPressed: () => setState(() => hide = !hide),
-                ),
+            InputField(
+              "Password",
+              pass,
+              obscure: hide,
+              icon: IconButton(
+                icon: Icon(hide ? Icons.visibility_off : Icons.visibility),
+                onPressed: () => setState(() => hide = !hide),
               ),
             ),
 
             const SizedBox(height: 20),
 
-            ElevatedButton(
-              onPressed: login,
-              child: const Text("Login"),
-            )
+            LoginButton(onPressed: login),
           ],
         ),
       ),
+    );
+  }
+}
+
+class InputField extends StatelessWidget {
+  final String label;
+  final TextEditingController controller;
+  final bool obscure;
+  final Widget? icon;
+
+  const InputField(
+    this.label,
+    this.controller, {
+    this.obscure = false,
+    this.icon,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      obscureText: obscure,
+      decoration: InputDecoration(
+        labelText: label,
+        border: const OutlineInputBorder(),
+        suffixIcon: icon,
+      ),
+    );
+  }
+}
+
+class LoginButton extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  const LoginButton({required this.onPressed, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: const Text("Login"),
     );
   }
 }
